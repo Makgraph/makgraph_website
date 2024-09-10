25933f9866f674137c4f32e233f7ef5d57c51ea9
Makgraph <milienmakenson3@gmail.com>
2024-09-10 00:01:48 -0400

    m

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "build", // Assure-toi que le répertoire de sortie est 'build'
     rollupOptions: {
       output: {
         manualChunks: {
